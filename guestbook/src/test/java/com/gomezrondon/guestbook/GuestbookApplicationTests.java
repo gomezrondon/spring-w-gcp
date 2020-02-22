b79ed877cee702f62cba6d0a5544f5cfa62c0865
cajgomez <Jgomez@cambridgeassociates.com>
2020-02-22 11:06:29 -0600

    2nd commit

---
+++ guestbook/src/test/java/com/gomezrondon/guestbook/GuestbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gomezrondon.guestbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
